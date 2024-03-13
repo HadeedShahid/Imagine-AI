853999fa1b5b545356fcb3e32b106c8cce62e9fc
Hadeed Shahid <hadeed.shahid08@gmail.com>
2024-03-13 20:51:14 +0500

    clerk auth complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
+import { ClerkProvider } from '@clerk/nextjs'
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ['400','500','600','700'], variable: '--font-ibm-plex' });
 
 export const metadata: Metadata = {
@@ -17,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
+    
   );
 }
