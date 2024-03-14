402354968c462938569481f9106dd7f2eff11624
Hadeed Shahid <hadeed.shahid08@gmail.com>
2024-03-14 22:20:26 +0500

    added sidebar menu items

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import Sidebar from '@/components/shared/Sidebar'
 
 const Layout = ({children}:{children: React.ReactNode}) => {
   return (
     <main className="root">
-        <div className='="root-container'>
-                {children}
-            <div className='wrapper'></div>
+      <Sidebar />
+      <div className='="root-container'>
+        <div className='wrapper'>
+          {children}
         </div>
+      </div>
     </main>
   )
 }
